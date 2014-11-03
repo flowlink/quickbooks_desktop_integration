3791183c3efea4b0d2790da4fb8e0590b907b2a1
Washington Luiz <huoxito@gmail.com>
2014-11-03 15:20:38 -0300

    ItemIventory query examples

---
+++ lib/quickbooks_desktop_integration/inventory.rb
@@ -0,0 +1,29 @@
+module QuickbooksDesktopIntegration
+  class Inventory
+    attr_reader :result, :records
+
+    def initialize(xml)
+      xml.slice! '<?xml version="1.0" ?>'
+      parser = Nori.new :strip_namespaces => true
+      envelope = parser.parse xml
+
+      response = envelope['Envelope']['Body']['receiveResponseXML']['response']
+      @result = response['QBXML']['QBXMLMsgsRs']['ItemInventoryQueryRs']
+
+      inventories = result['ItemInventoryRet']
+      @records = inventories.is_a?(Array) ? inventories : [inventories].compact
+    end
+
+    def mapped_records
+      records.each do |record|
+        {
+          id: record['Name'],
+          sku: record['Name'],
+          product_id: record['Name'],
+          quantity: record['QuantityOnHand']
+          quickbooks: record
+        }
+      end
+    end
+  end
+end
