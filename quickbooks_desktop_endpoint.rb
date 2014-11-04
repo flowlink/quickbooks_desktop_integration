5ea8eaee4b321712ffc4d776efc7f2912406672e
Reinaldo Braga <reinaldob@gmail.com>
2014-11-04 15:11:33 -0200

    IN-396 Adds add/update products

--- quickbooks_desktop_endpoint.rb
+++ quickbooks_desktop_endpoint.rb
@@ -68,4 +68,22 @@ class QuickbooksDesktopEndpoint < EndpointBase::Sinatra::Base
       result 200
     end
   end
+
+  post %r{(add_product|update_product)$} do
+    # TODO Drop the hardcoded account id ..
+    config = { account_id: 'x123', origin: 'quickbooks' }
+    payload = { products: {} }
+
+    integration = QuickbooksDesktopIntegration::Base.new config, payload
+    products = integration.start_processing "integrated"
+
+    if products.any?
+      count = products.count
+      add_value 'products', products
+      result 200, "#{"product".pluralize count} stored to send to Quickbooks Desktop"
+    else
+      result 200
+    end
+  end
+
 end
