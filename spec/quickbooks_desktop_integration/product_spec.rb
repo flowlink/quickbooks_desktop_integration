f589fefe5e6ca97bd605a56f3b193a02e1068c56
Reinaldo Braga <reinaldob@gmail.com>
2014-11-04 17:37:26 -0200

    IN-296 Adds spec for products integration

--- spec/quickbooks_desktop_integration/product_spec.rb
+++ spec/quickbooks_desktop_integration/product_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 module QuickbooksDesktopIntegration
   describe Product do
     it "builds wombat products from xml response" do
-      subject.convert_xml Factory.item_query_rs_qbxml
+      subject = Product.new Factory.item_inventory_add_rs_qbxml
+      expect(subject.mapped_records.size).to eq(1)
+      expect(subject.mapped_records[0][:id]).to eq('SPREE-T-SHIRT-1')
     end
   end
 end
