5587f07c137e5c3d64c47920d939bdae88dab06b
Pablo Cantero <pablo@pablocantero.com>
2014-11-04 17:50:39 -0200

    Add real ItemQueryRs response

--- spec/support/factory.rb
+++ spec/support/factory.rb
@@ -1,20 +1,28 @@
 module Factory
+  @@cache = {}
+
   class << self
     Dir.entries("#{File.dirname(__FILE__)}/payloads").each do |file_name|
-      next if file_name == '.' or file_name == '..'
-      name, ext = file_name.split(".", 2)
+      next if file_name.start_with? '.'
 
-      define_method name do
-        JSON.parse(IO.read("#{File.dirname(__FILE__)}/payloads/#{name}.json")).with_indifferent_access
+      name = file_name.split('.', 2).first
+
+      method_name = "#{name}_qbxml".downcase
+
+      define_method method_name do
+        @@cache[method_name] ||= JSON.parse(IO.read("#{File.dirname(__FILE__)}/payloads/#{name}.json")).with_indifferent_access
       end
     end
 
     Dir.entries("#{File.dirname(__FILE__)}/qbxml_examples").each do |file_name|
-      next if file_name == '.' or file_name == '..'
-      name, ext = file_name.split(".", 2)
+      next if file_name.start_with? '.'
 
-      define_method "#{name}_qbxml" do
-        IO.read("#{File.dirname(__FILE__)}/qbxml_examples/#{name}.xml")
+      name = file_name.split('.', 2).first
+
+      method_name = "#{name}_qbxml".downcase
+
+      define_method method_name do
+        @@cache[method_name] ||= CGI.unescapeHTML(IO.read("#{File.dirname(__FILE__)}/qbxml_examples/#{name}.xml"))
       end
     end
   end
