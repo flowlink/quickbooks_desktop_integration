b5edb8fc799be3f8bf80803c68f060ac5b6a486c
Washington Luiz <huoxito@gmail.com>
2014-11-05 00:51:43 -0300

    drop useless spec

--- spec/quickbooks_desktop_integration/order_spec.rb
+++ spec/quickbooks_desktop_integration/order_spec.rb
@@ -2,15 +2,5 @@ require 'spec_helper'
 
 module QuickbooksDesktopIntegration
   describe Order do
-    let(:orders) { Factory.orders }
-    let(:config) { { connection_id: 'x123' } }
-
-    subject { described_class.new config, orders }
-
-    it "process orders" do
-      VCR.use_cassette "order/1414614890" do
-        subject.start_processing
-      end
-    end
   end
 end
