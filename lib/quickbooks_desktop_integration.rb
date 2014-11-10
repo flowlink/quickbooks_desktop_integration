12530c99345d0e304d28dfc16883a1c6c68820b1
Reinaldo Braga <reinaldob@gmail.com>
2014-11-10 09:11:14 -0200

    IN-396 Changes import files to the new structure

--- lib/quickbooks_desktop_integration.rb
+++ lib/quickbooks_desktop_integration.rb
@@ -4,12 +4,10 @@ require 'quickbooks_desktop_helper'
 
 require 'persistence/s3_util'
 require 'persistence/object'
-require 'quickbooks_desktop_integration/order'
-require 'quickbooks_desktop_integration/inventory'
-require 'quickbooks_desktop_integration/product'
 
 require 'qbwc/response/all'
 require 'qbwc/request/customer'
+require 'qbwc/request/product'
 
 require 'qbwc/consumer'
 require 'qbwc/producer'
