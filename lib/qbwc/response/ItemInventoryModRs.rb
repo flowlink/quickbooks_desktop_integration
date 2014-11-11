d248262d452493de4a9c94a60788875348085267
Reinaldo Braga <reinaldob@gmail.com>
2014-11-11 09:43:48 -0200

    IN-396 Adds update results

---
+++ lib/qbwc/response/ItemInventoryModRs.rb
@@ -0,0 +1,41 @@
+module QBWC
+  module Response
+    class ItemInventoryModRs
+      attr_reader :records
+
+      def initialize(records)
+        @records = records
+      end
+
+      def process
+        return { 'statuses_objects' => nil } if records.empty?
+
+
+        # TODO Error handling
+
+        # &lt;QBXML&gt;
+        # &lt;QBXMLMsgsRs&gt;
+        # &lt;ItemInventoryAddRs statusCode="3100" statusSeverity="Error" statusMessage="The name &amp;quot;SPREE-T-SHIRT697877&amp;quot; of the list element is already in use." /&gt;
+        # &lt;/QBXMLMsgsRs&gt;
+        # &lt;/QBXML&gt;
+        # </response><hresult /><message /></receiveResponseXML></soap:Body></soap:Envelope>
+
+puts " \n\n\n **** Records: #{records.inspect} \n\n"
+
+        products = []
+        records.each do |object|
+          products << { :products => {
+                                       :id            => object['Name'],
+                                       :list_id       => object['ListID'],
+                                       :edit_sequence => object['EditSequence']
+                                      }
+                      }
+        end
+
+        { :statuses_objects => { :processed => products, :failed => [] } }.with_indifferent_access
+
+        #Move files and create  notifications
+      end
+    end
+  end
+end
