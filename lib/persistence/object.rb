c92ca16f1084e113a6a87a27df7c7f08dbb89653
Reinaldo Braga <reinaldob@gmail.com>
2014-11-11 12:48:33 -0200

    IN-396 Fixes gsub to list_id for insert

--- lib/persistence/object.rb
+++ lib/persistence/object.rb
@@ -128,7 +128,8 @@ module Persistence
       collection.with_prefix(prefix).enum.map do |s3_object|
         connection_id, folder, filename = s3_object.key.split('/')
         object_type, file_name, edit_sequence, list_id = filename.split('_')
-        # list_id.gsub!('.csv','')
+
+        list_id.gsub!('.csv','') unless list_id.nil?
 
         contents = s3_object.read
 
