7d10c68af2800506d99332413740881f4b92fa18
Washington Luiz <huoxito@gmail.com>
2014-11-12 00:03:54 -0300

    moar cassetes

--- spec/persistence/settings_spec.rb
+++ spec/persistence/settings_spec.rb
@@ -3,11 +3,5 @@ require 'spec_helper'
 module Persistence
   describe Settings do
     subject { described_class.new connection_id: '54591b3a5869632afc090000' }
-
-    it "fetch existing config files" do
-      VCR.use_cassette "settings/45435534253425" do
-        configs = subject.fetch
-      end
-    end
   end
 end
