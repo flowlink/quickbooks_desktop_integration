11973759808f8c9b440de6dba776d8d84d0fd20a
Reinaldo Braga <reinaldob@gmail.com>
2014-11-20 15:34:32 -0200

    IN-396 Fixes cassetes from orders changes

--- spec/qbwc/request/orders_spec.rb
+++ spec/qbwc/request/orders_spec.rb
@@ -3,17 +3,20 @@ require 'spec_helper'
 module QBWC
   module Request
     describe Orders do
+      before do
+        allow_any_instance_of(Persistence::Object).to receive(:save_session).and_return("82bfb8e5-99e3-41c9-a4cc-19a0001b6ecf")
+      end
+
       subject { described_class }
 
       it "builds xml request from orders" do
         orders = Factory.orders['orders']
-        xml = subject.generate_request_insert_update orders
-
-        expect(xml).to match /CustomerAddRq/
-        expect(xml).to match /ItemInventoryAddRq/
-        expect(xml).to match /SalesOrderAdd/
-        expect(xml).to match /SalesOrderLineAdd/
-        expect(xml).to match /100.00/
+        VCR.use_cassette "requests/insert_update_orders" do
+          xml = subject.generate_request_insert_update orders
+          expect(xml).to match /SalesOrderAdd/
+          expect(xml).to match /SalesOrderLineAdd/
+          expect(xml).to match /100.00/
+        end
       end
     end
   end
