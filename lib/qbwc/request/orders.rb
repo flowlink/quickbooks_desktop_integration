276a9e20ddb8385c9ef458e509c0abe777262f64
Reinaldo Braga <reinaldob@gmail.com>
2014-11-23 14:30:43 -0200

    IN-396 Adds items to update sales order and sales receipt

--- lib/qbwc/request/orders.rb
+++ lib/qbwc/request/orders.rb
@@ -23,34 +23,36 @@ module QBWC
 
         def search_xml(order_id, session_id)
          <<-XML
-          <SalesOrderQueryRq requestID="#{session_id}">
-            <RefNumberCaseSensitive>#{order_id}</RefNumberCaseSensitive>
-          </SalesOrderQueryRq>
+<SalesOrderQueryRq requestID="#{session_id}">
+  <RefNumberCaseSensitive>#{order_id}</RefNumberCaseSensitive>
+  <IncludeLineItems>true</IncludeLineItems>
+</SalesOrderQueryRq>
           XML
         end
 
         def add_xml_to_send(record, params= {}, session_id)
           <<-XML
-            <SalesOrderAddRq requestID="#{session_id}">
-              <SalesOrderAdd>
-                #{sales_order record, params}
-                #{record['line_items'].map { |l| sales_order_line_add l }.join("")}
-              </SalesOrderAdd>
-            </SalesOrderAddRq>
+
+<SalesOrderAddRq requestID="#{session_id}">
+  <SalesOrderAdd>
+    #{sales_order record, params}
+    #{record['line_items'].map { |l| sales_order_line_add l }.join("")}
+  </SalesOrderAdd>
+</SalesOrderAddRq>
           XML
         end
 
         def update_xml_to_send(record, params= {}, session_id)
-          #{record['line_items'].map { |l| sales_order_line_mod l }.join("")}
-
           <<-XML
-            <SalesOrderModRq requestID="#{session_id}">
-              <SalesOrderMod>
-                <TxnID>#{record['list_id']}</TxnID>
-                <EditSequence>#{record['edit_sequence']}</EditSequence>
-                #{sales_order record, params}
-              </SalesOrderMod>
-            </SalesOrderModRq>
+
+<SalesOrderModRq requestID="#{session_id}">
+  <SalesOrderMod>
+    <TxnID>#{record['list_id']}</TxnID>
+    <EditSequence>#{record['edit_sequence']}</EditSequence>
+    #{sales_order record, params}
+    #{record['line_items'].map { |l| sales_order_line_mod l }.join("")}
+  </SalesOrderMod>
+</SalesOrderModRq>
           XML
         end
 
@@ -73,55 +75,60 @@ module QBWC
         #
         def sales_order(record, params)
           <<-XML
-            <CustomerRef>
-              <FullName>#{record['email']}</FullName>
-            </CustomerRef>
-            <TxnDate>#{Time.parse(record['placed_on']).to_date}</TxnDate>
-            <RefNumber>#{record['id']}</RefNumber>
-            <BillAddress>
-              <Addr1>#{record['billing_address']['address1']}</Addr1>
-              <Addr2>#{record['billing_address']['address2']}</Addr2>
-              <City>#{record['billing_address']['city']}</City>
-              <State>#{record['billing_address']['state']}</State>
-              <PostalCode>#{record['billing_address']['zipcode']}</PostalCode>
-              <Country>#{record['billing_address']['country']}</Country>
-            </BillAddress>
-            <ShipAddress>
-              <Addr1>#{record['shipping_address']['address1']}</Addr1>
-              <Addr2>#{record['shipping_address']['address2']}</Addr2>
-              <City>#{record['shipping_address']['city']}</City>
-              <State>#{record['shipping_address']['state']}</State>
-              <PostalCode>#{record['shipping_address']['zipcode']}</PostalCode>
-              <Country>#{record['shipping_address']['country']}</Country>
-            </ShipAddress>
+
+    <CustomerRef>
+      <FullName>#{record['email']}</FullName>
+    </CustomerRef>
+    <TxnDate>#{Time.parse(record['placed_on']).to_date}</TxnDate>
+    <RefNumber>#{record['id']}</RefNumber>
+    <BillAddress>
+      <Addr1>#{record['billing_address']['address1']}</Addr1>
+      <Addr2>#{record['billing_address']['address2']}</Addr2>
+      <City>#{record['billing_address']['city']}</City>
+      <State>#{record['billing_address']['state']}</State>
+      <PostalCode>#{record['billing_address']['zipcode']}</PostalCode>
+      <Country>#{record['billing_address']['country']}</Country>
+    </BillAddress>
+    <ShipAddress>
+      <Addr1>#{record['shipping_address']['address1']}</Addr1>
+      <Addr2>#{record['shipping_address']['address2']}</Addr2>
+      <City>#{record['shipping_address']['city']}</City>
+      <State>#{record['shipping_address']['state']}</State>
+      <PostalCode>#{record['shipping_address']['zipcode']}</PostalCode>
+      <Country>#{record['shipping_address']['country']}</Country>
+    </ShipAddress>
           XML
         end
 
         def sales_order_line_add(line)
           <<-XML
-            <SalesOrderLineAdd>
-              #{sales_order_line(line)}
-            </SalesOrderLineAdd>
+
+    <SalesOrderLineAdd>
+      #{sales_order_line(line)}
+    </SalesOrderLineAdd>
           XML
         end
 
         def sales_order_line_mod(line)
           <<-XML
-            <SalesOrderLineMod>
-              #{sales_order_line(line)}
-            </SalesOrderLineMod>
+
+    <SalesOrderLineMod>
+      <TxnLineID>#{line['txn_line_id']}</TxnLineID>
+      #{sales_order_line(line)}
+    </SalesOrderLineMod>
           XML
         end
 
         def sales_order_line(line)
           <<-XML
-              <ItemRef>
-                <FullName>#{line['product_id']}</FullName>
-              </ItemRef>
-              <Quantity>#{line['quantity']}</Quantity>
-              <!-- <Amount>#{'%.2f' % line['price'].to_f}</Amount> -->
-              <Rate>#{line['price']}</Rate>
-              #{tax_code_line(line)}
+
+      <ItemRef>
+        <FullName>#{line['product_id']}</FullName>
+      </ItemRef>
+      <Quantity>#{line['quantity']}</Quantity>
+      <!-- <Amount>#{'%.2f' % line['price'].to_f}</Amount> -->
+      <Rate>#{line['price']}</Rate>
+      #{tax_code_line(line)}
           XML
         end
 
@@ -129,9 +136,10 @@ module QBWC
           return '' if line['tax_code_id'].to_s.empty?
 
           <<-XML
-            <SalesTaxCodeRef>
-              <FullName>#{line['tax_code_id']}</FullName>
-            </SalesTaxCodeRef>
+
+      <SalesTaxCodeRef>
+        <FullName>#{line['tax_code_id']}</FullName>
+      </SalesTaxCodeRef>
           XML
         end
 
--- lib/qbwc/request/returns.rb
+++ lib/qbwc/request/returns.rb
@@ -25,6 +25,7 @@ module QBWC
          <<-XML
           <SalesReceiptQueryRq requestID="#{session_id}">
             <RefNumberCaseSensitive>#{return_id}</RefNumberCaseSensitive>
+            <IncludeLineItems>true</IncludeLineItems>
           </SalesReceiptQueryRq>
           XML
         end
@@ -41,14 +42,13 @@ module QBWC
         end
 
         def update_xml_to_send(record, params= {}, session_id)
-          #{record['items'].map { |l| sales_receipt_line_mod l }.join("")}
-
           <<-XML
             <SalesReceiptModRq requestID="#{session_id}">
               <SalesReceiptMod>
                 <TxnID>#{record['list_id']}</TxnID>
                 <EditSequence>#{record['edit_sequence']}</EditSequence>
                 #{sales_receipt record, params}
+                #{record['items'].map { |l| sales_receipt_line_mod l }.join("")}
               </SalesReceiptMod>
             </SalesReceiptModRq>
           XML
@@ -114,6 +114,7 @@ module QBWC
         def sales_receipt_line_mod(line)
           <<-XML
             <SalesReceiptLineMod>
+              <TxnLineID>#{line['txn_line_id']}</TxnLineID>
               #{sales_receipt_line(line)}
             </SalesReceiptLineMod>
           XML
