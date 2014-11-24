e9513f87dd8eeba693343e3e8ab2db98476b6cd3
Reinaldo Braga <reinaldob@gmail.com>
2014-11-23 23:26:39 -0200

    IN-396 Changes map from producer to prevent errors from more than one return on a query

--- lib/qbwc/response/all.rb
+++ lib/qbwc/response/all.rb
@@ -29,10 +29,10 @@ module QBWC
 
           class_name = "QBWC::Response::#{key}".constantize
           value = value.is_a?(Hash)? [value] : Array(value)
+          #value.map(&:values).flatten.select { |value| value.is_a?(Hash) }
 
-          # records = value.map(&:values).flatten.select { |value| value.is_a?(Hash) }
-          records = value.map{ |item| item.values.select { |value| value.is_a?(Hash) }.
-                        map{ |sub| sub.merge({ 'request_id' => item['@requestID'] })
+          records = value.map{ |item| item.values.flatten.select { |value| value.is_a?(Hash) }.flatten.
+                              map{ |sub| sub.merge({ 'request_id' => item['@requestID'] })
                            }
                }.flatten
 
