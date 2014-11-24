395aaba8a545d759f967b9738f3422bcd698047d
Reinaldo Braga <reinaldob@gmail.com>
2014-11-24 10:24:44 -0200

    IN-396 Updating cassettes and fixing specs

--- spec/qbwc/request/inventories_spec.rb
+++ spec/qbwc/request/inventories_spec.rb
@@ -5,9 +5,13 @@ module QBWC
     describe Inventories do
       subject { described_class }
 
+      before do
+        allow_any_instance_of(Persistence::Object).to receive(:save_session).and_return("1f8d3ff5-6f6c-43d6-a084-0ac95e2e29ad")
+      end
+
       it "parses timestamp and return request xml" do
         time = Time.now.utc.to_s
-        xml = subject.polling_xml(time)
+        xml = subject.polling_xml(time, {})
         expect(xml).to match time.split.first
       end
     end
