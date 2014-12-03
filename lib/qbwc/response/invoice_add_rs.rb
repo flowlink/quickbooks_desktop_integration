e809d297a92a2a4950def5f9eed19b3accf2febf
Reinaldo Braga <reinaldob@gmail.com>
2014-12-03 08:18:32 -0200

    IN-396 Fixes error handling for shipments

--- lib/qbwc/response/invoice_add_rs.rb
+++ lib/qbwc/response/invoice_add_rs.rb
@@ -8,6 +8,14 @@ module QBWC
       end
 
       def handle_error(errors, config)
+        errors.each do |error|
+          Persistence::Object.handle_error(
+            config,
+            error.merge(context: 'Adding Shipments'),
+            "shipments",
+            error[:request_id]
+          )
+        end
       end
 
       def process(config = {})
