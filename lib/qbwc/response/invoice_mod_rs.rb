2eb479bd13ed01e8d3db7ce4467bb83bbab5879a
Reinaldo Braga <reinaldob@gmail.com>
2014-12-20 17:18:43 -0200

    Adds invoice mod request processor

---
+++ lib/qbwc/response/invoice_mod_rs.rb
@@ -0,0 +1,38 @@
+module QBWC
+  module Response
+    class InvoiceModRs
+      attr_reader :records
+
+      def initialize(records)
+        @records = records
+      end
+
+      def handle_error(errors, config)
+        errors.each do |error|
+          Persistence::Object.handle_error(
+            config,
+            error.merge(context: 'Updating Shipments'),
+            "shipments",
+            error[:request_id]
+          )
+        end
+      end
+
+      def process(config = {})
+        return { statuses_objects: nil }.with_indifferent_access if records.empty?
+
+        objects = records.map do |object|
+          {
+            shipments: {
+              id: object['PONumber'],
+              list_id: object['TxnID'],
+              edit_sequence: object['EditSequence']
+            }
+          }
+        end
+
+        Persistence::Object.update_statuses(config, objects)
+      end
+    end
+  end
+end
