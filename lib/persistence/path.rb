4c4c8acdd8dcf6c37ee5847377c93513a3f22434
Reinaldo Braga <reinaldob@gmail.com>
2015-03-26 08:54:21 -0300

    Extracts path from persistence to reuse in other classes

---
+++ lib/persistence/path.rb
@@ -0,0 +1,38 @@
+module Persistence
+  # Path for S3 storage
+  class Path
+    attr_reader :config
+
+    def initialize(config)
+      @config = config
+    end
+
+    def two_phase_pending
+      "#{config[:origin]}_two_phase_pending"
+    end
+
+    def sessions
+      "#{config[:origin]}_sessions"
+    end
+
+    def base_name
+      "#{config[:connection_id]}"
+    end
+
+    def pending
+      "#{config[:origin]}_pending"
+    end
+
+    def ready
+      "#{config[:origin]}_ready"
+    end
+
+    def processed
+      "#{config[:origin]}_processed"
+    end
+
+    def failed
+      "#{config[:origin]}_failed"
+    end
+  end
+end
