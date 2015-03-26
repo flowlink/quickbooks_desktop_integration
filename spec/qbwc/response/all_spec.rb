322495321f80f07faed0cc4ba1c2f1a809940bc0
Reinaldo Braga <reinaldob@gmail.com>
2015-03-26 15:19:43 -0300

    Removes unused spec

--- spec/qbwc/response/all_spec.rb
+++ spec/qbwc/response/all_spec.rb
@@ -8,12 +8,5 @@ describe QBWC::Response::All do
       expect_any_instance_of(QBWC::Response::ItemInventoryQueryRs).to receive(:process)
       subject.process
     end
-
-    xit "process sales order add rq fine" do
-      VCR.use_cassette "response/process_sales" do
-        subject = described_class.new Factory.sales_order_add_rs_raw_qbxml_raw
-        subject.process
-      end
-    end
   end
 end
