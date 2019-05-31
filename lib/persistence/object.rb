7bdbf14285602c3ddd4531a78e7bd0e55c0335b4
Bruno Gianni <bcgianni@gmail.com>
2019-05-31 16:19:30 -0300

    Use symbols instead

--- lib/persistence/object.rb
+++ lib/persistence/object.rb
@@ -599,7 +599,7 @@ module Persistence
           payments = QBWC::Request::Orders.build_payments_from_order(object)
           payments.each do |payment|
             puts payment
-            next unless (payment['id'] && payment['customer'] && payment['amount'] && payment['payment_method'])  
+            next unless (payment[:id] && payment[:customer] && payment[:amount] && payment[:payment_method])  
             file = "#{path.base_name}/#{path.two_phase_pending}/payments_#{id_of_object(object)}_.json"
             amazon_s3.export file_name: file, objects: [payment]
           end
