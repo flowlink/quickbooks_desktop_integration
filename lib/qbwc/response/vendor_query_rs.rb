024cf3366d30abee88d5f41d63543bce73a59d81
Bruno <bcgianni@gmail.com>
2019-06-28 13:40:23 -0300

    Add missing flow vendors

--- lib/qbwc/response/vendor_query_rs.rb
+++ lib/qbwc/response/vendor_query_rs.rb
@@ -19,8 +19,26 @@ module QBWC
       def process(config)
         return if records.empty?
 
-        puts records.inspect
+        puts "Config for customer query: #{config}"
 
+        receive_configs = config[:receive] || []
+        vendor_params = receive_configs.find { |c| c['vendors'] }
+
+        if vendor_params
+          payload = { vendors: to_flowlink }
+          config = { origin: 'quickbooks' }.merge config.reject{|k,v| k == :origin || k == "origin"}
+
+          poll_persistence = Persistence::Polling.new(config, payload)
+          poll_persistence.save_for_polling
+
+          customer_params['customers']['quickbooks_since'] = last_time_modified
+          customer_params['customers']['quickbooks_force_config'] = 'true'
+
+          # Override configs to update timestamp so it doesn't keep geting the
+          # same inventories
+          params = customer_params['customers']
+          Persistence::Settings.new(params.with_indifferent_access).setup
+        end
         config  = { origin: 'flowlink', connection_id: config[:connection_id]  }
 
         Persistence::Object.new(config, {}).update_objects_with_query_results(objects_to_update)
@@ -29,6 +47,11 @@ module QBWC
       end
 
       private
+
+      def last_time_modified
+        time = records.sort_by { |r| r['TimeModified'] }.last['TimeModified'].to_s
+        Time.parse(time).in_time_zone('Pacific Time (US & Canada)').iso8601
+      end
 
       def objects_to_update
         records.map do |record|
