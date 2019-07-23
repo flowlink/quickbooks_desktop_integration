2ea92bab4f2a60b034393c33adebc254535c5f31
Marc Salo <marc@nurelm.com>
2019-07-23 09:07:14 -0400

    Added logging

--- lib/qbwc/request/journals.rb
+++ lib/qbwc/request/journals.rb
@@ -21,7 +21,12 @@ module QBWC
         def build_request_by_action(object, params, session_id)
           if object['list_id'].to_s.empty? && object['action'] != "DELETE"
             puts "ADD"
-            add_xml_to_send(object, params, session_id)
+            testing = add_xml_to_send(object, params, session_id)
+            puts "-journal-" * 6
+            puts ""
+            puts testing
+            puts ""
+            puts "-journal-" * 6
           elsif object['action'] == "DELETE"
             puts "DELETE"
             delete_xml_to_send(object, session_id)
@@ -54,7 +59,7 @@ module QBWC
           <<-XML
             <JournalEntryAddRq requestID="#{session_id}">
                <JournalEntryAdd>
-                #{journal_xml(journal, params)}
+                #{journal_xml(journal, params, false)}
                </JournalEntryAdd>
             </JournalEntryAddRq>
           XML
@@ -68,7 +73,7 @@ module QBWC
                <JournalEntryMod>
                   <TxnID>#{journal['list_id']}</TxnID>
                   <EditSequence>#{journal['edit_sequence']}</EditSequence>
-                  #{journal_xml(journal, params)}
+                  #{journal_xml(journal, params, true)}
                </JournalEntryMod>
             </JournalEntryModRq>
           XML
@@ -83,12 +88,11 @@ module QBWC
           XML
         end
 
-        def journal_xml(journal, params)
+        def journal_xml(journal, params, isAdjustment)
           <<-XML
               <TxnDate>#{Time.parse(journal['journal_date']).to_date}</TxnDate>
               <RefNumber>#{journal['id']}</RefNumber>
-              <IsAdjustment>false</IsAdjustment>
-              <IsHomeCurrencyAdjustment>true</IsHomeCurrencyAdjustment>
+              <IsAdjustment>#{isAdjustment}</IsAdjustment>
               #{journal['line_items'].map { |j| journal_line_items j }.join('')}
           XML
         end
@@ -111,25 +115,20 @@ module QBWC
 
         def fill_line_item(item, amount)
           <<-XML
-            <!-- TxnLineID is required for update. Might need to be set to -1 on create?? -->
             #{item['line_id'] ? "<TxnLineID>#{item['line_id']}</TxnLineID>" : ''}
             <AccountRef>
-                <!-- <ListID>IDTYPE</ListID> -->
                 <FullName>#{item['account_description']}</FullName>
             </AccountRef>
             <Amount>#{amount}</Amount>
             <Memo>#{item['description']}</Memo>
             #{item['customer'] ? fill_customer(item['customer']) : ''}
             #{item['class'] ? fill_class(item['class']) : ''}
-            <!-- BillableStatus may have one of the following values: Billable, NotBillable, HasBeenBilled -->
-            <!-- <BillableStatus>'Billable'</BillableStatus> -->
           XML
         end
 
         def fill_customer(name)
           <<-XML
             <EntityRef>
-                <!-- <ListID>IDTYPE</ListID> -->
                 <FullName>#{name}</FullName>
             </EntityRef>
           XML
@@ -138,7 +137,6 @@ module QBWC
         def fill_class(name)
           <<-XML
             <ClassRef>
-                <!-- <ListID>IDTYPE</ListID> -->
                 <FullName>#{name}</FullName>
             </ClassRef>
           XML
