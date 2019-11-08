4f784bb18f59885bb953d6fca3f890b73caa5aca
Marc Salo <marc@nurelm.com>
2019-11-08 11:06:37 -0500

    logging

--- lib/qbwc/request/customers.rb
+++ lib/qbwc/request/customers.rb
@@ -72,7 +72,7 @@ module QBWC
 
             config = { connection_id: params['connection_id'] }.with_indifferent_access
             session_id = Persistence::Session.save(config, object)
-            puts "HERE - #{add_fields(object['billing_address'], ADDRESS_MAP).gsub(/\s+/, "") if object['billing_address']}"
+            puts "HERE - #{add_fields(object['billing_address'], ADDRESS_MAP).gsub(/\s+/, "")}"
 
             request << (object[:list_id].to_s.empty? ? add_xml_to_send(object, session_id, config) : update_xml_to_send(object, session_id, config))
           end
