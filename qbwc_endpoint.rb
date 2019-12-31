07a9b045f391e9a7eef48c4e73c4a79a3d41e568
bcgianni <bcgianni@gmail.com>
2019-12-31 13:26:54 -0300

    Fix encoding

--- qbwc_endpoint.rb
+++ qbwc_endpoint.rb
@@ -133,7 +133,7 @@ class QBWCEndpoint < Sinatra::Base
 </QBXML>
     XML
 
-    puts @qbxml.gsub("\n", '').encode(Encoding::US-ASCII)
+    puts @qbxml.gsub("\n", '').encode(Encoding.find("US-ASCII"))
     erb :'qbwc/send_request_xml'
   end
 
