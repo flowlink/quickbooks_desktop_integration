052728a8252f4b69d2ffbb0d13da2e88bcaa1161
Marc Salo <marc@nurelm.com>
2020-01-07 14:51:52 -0500

    Adding logging

--- lib/qbwc/request/invoices.rb
+++ lib/qbwc/request/invoices.rb
@@ -276,7 +276,7 @@ module QBWC
 
           line['use_amount'] = true if params['use_amount_for_tax'].to_s == "1"
           puts params['connection_id']
-          puts "Adding Tax... Should we use amount? #{params['use_amount_for_tax']} - so line is now: #{line}" if params['connection_id'] == "oilsolutionsgroup"
+          puts "Adding Tax...Should we use amount? #{params['use_amount_for_tax']} - so line is now: #{line}" if params['connection_id'] == "oilsolutionsgroup"
 
 
           invoice_line_add line
