ed4dd2a4547ce42f2bf7d369e2c235687c13f2c0
Raymond Sapida <ray.sapida@gmail.com>
2020-04-01 12:30:44 -0700

    Start adding specs to settings_spec.rb
    - Adding s3 in spec_helper to enable global stubbing of s3 methods

--- spec/persistence/settings_spec.rb
+++ spec/persistence/settings_spec.rb
@@ -2,6 +2,28 @@ require 'spec_helper'
 
 module Persistence
   describe Settings do
-    subject { described_class.new connection_id: '54591b3a5869632afc090000' }
+    let(:connection_id) { "nurelmremote" }
+    subject {
+      described_class.new({ connection_id: connection_id })
+    }
+
+    describe 'reader methods' do
+      it '#base_name' do
+        expect(subject.base_name).to eq "#{connection_id}/settings"
+      end
+    end
+
+    describe '#setup' do
+      it 'returns nil when no extra flows to generate' do
+        expect(subject.setup).to be nil
+      end
+    end
+
+    describe '#settings' do
+      it 'returns an array with prefix get_' do
+        expect(subject.settings('get_')).to be_instance_of Array
+      end
+    end
+
   end
 end
