22f5d7f70069ed5f51012c4dd82be158d3f7a7e2
Raymond Sapida <ray.sapida@gmail.com>
2020-04-08 10:20:54 -0700

    Add remove_backslash in the filename

--- lib/persistence/object.rb
+++ lib/persistence/object.rb
@@ -172,8 +172,8 @@ module Persistence
 
       objects_to_be_renamed.to_a.compact.each do |object|
         puts "-" * 99
-        filename     = "#{prefix}/#{object[:object_type].pluralize}_#{object[:object_ref]}_"
-        filename_with_bucket = "#{prefix_with_bucket}/#{object[:object_type].pluralize}_#{object[:object_ref]}_"
+        filename     = "#{prefix}/#{object[:object_type].pluralize}_#{remove_backslash(object[:object_ref])}_"
+        filename_with_bucket = "#{prefix_with_bucket}/#{object[:object_type].pluralize}_#{remove_backslash(object[:object_ref])}_"
 
         puts filename_with_bucket.inspect
         # TODO what if the file is not there? we should probably at least
