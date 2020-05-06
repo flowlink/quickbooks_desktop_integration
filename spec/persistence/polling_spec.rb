c3edf9916da7c3969af541ee88632e07d132c199
Raymond Sapida <ray.sapida@gmail.com>
2020-05-06 12:26:42 -0700

    Update `#save_for_polling_without_timestamp` to concatenate objects
    - The method will read from the key first and try to merge it with the
    objects being given
    - Update s3_util to not delete a file after reading it. Also looks like
    it wasn't being used because a NoMethodError error would have been thrown

--- spec/persistence/polling_spec.rb
+++ spec/persistence/polling_spec.rb
@@ -16,13 +16,31 @@ module Persistence
       end
     end
 
-    it '#save_for_polling_without_timestamp' do
-      payload = { products: [] }
-      config = { origin: 'quickbooks', connection_id: 'nurelmremote' }
-      VCR.use_cassette 'persistence/save_for_polling_without_timestamp' do
-        subject = described_class.new(config, payload)
-        s3 = subject.save_for_polling_without_timestamp
-        expect(s3.key).to match "#{config[:connection_id]}/quickbooks_pending/#{payload.keys.first}_"
+    context '#save_for_polling_without_timestamp' do
+      it 'saves a file in s3 bucket' do
+        payload = { products: [] }
+        config = { origin: 'quickbooks', connection_id: 'nurelmremote' }
+        VCR.use_cassette 'persistence/save_for_polling_without_timestamp' do
+          subject = described_class.new(config, payload)
+          s3 = subject.save_for_polling_without_timestamp
+          expect(s3.key).to match "#{config[:connection_id]}/quickbooks_pending/#{payload.keys.first}_"
+        end
+      end
+
+      it 'merges products' do
+        # Already in the s3 bucket
+        products = Factory.products
+
+        product = Factory.product_single
+        payload = { products: [product] }
+        config = { origin: 'quickbooks', connection_id: 'nurelmremote' }
+        VCR.use_cassette 'persistence/save_for_polling_without_timestamp_merging' do
+          subject = described_class.new(config, payload)
+          s3 = subject.save_for_polling_without_timestamp
+          result = JSON.parse(s3.get.body.read)
+          expect(s3.key).to match "#{config[:connection_id]}/quickbooks_pending/#{payload.keys.first}_"
+          expect(result.size).to eq 2
+        end
       end
     end
 
