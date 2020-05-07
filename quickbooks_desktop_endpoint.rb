1c2e499268361b6866a343512e2e5a2c0610cbd6
Marc Salo <marc@nurelm.com>
2020-05-07 10:45:19 -0400

    try again :

--- quickbooks_desktop_endpoint.rb
+++ quickbooks_desktop_endpoint.rb
@@ -91,14 +91,13 @@ class QuickbooksDesktopEndpoint < EndpointBase::Sinatra::Base
 
       Persistence::Settings.new(config).setup
 
-      already_has_guid?
-      generate_and_add_guid unless @already_has_guid
-      return_payload = add_flow_return_payload
-
+      unless already_has_guid?
+        generate_and_add_guid
+        add_object determine_name(object_type).singularize, add_flow_return_payload
+      end
+      
       integration = Persistence::Object.new(config, @payload)
       integration.save
-
-      add_object determine_name(integration.payload_key).singularize, return_payload unless @already_has_guid
 
       object_type = integration.payload_key.capitalize
       result 200, "#{object_type} waiting for Quickbooks Desktop scheduler"
@@ -237,7 +236,7 @@ class QuickbooksDesktopEndpoint < EndpointBase::Sinatra::Base
   end
 
   def already_has_guid?
-    @already_has_guid ||= @payload[object_type][:external_guid] && @payload[object_type][:external_guid] != ""
+    @payload[object_type][:external_guid] && @payload[object_type][:external_guid] != ""
   end
 
   # NOTE this lives in endpoint_base. Added here just so it's closer ..
