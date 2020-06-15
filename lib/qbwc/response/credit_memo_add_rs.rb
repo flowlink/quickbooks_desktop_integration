7725164cba3371af23266496e8807cb321408aee
Raymond Sapida <ray.sapida@gmail.com>
2020-06-14 20:34:54 -0700

    Add classes for the response for credit memo add and mod

---
+++ lib/qbwc/response/credit_memo_add_rs.rb
@@ -0,0 +1,39 @@
+module QBWC
+  module Response
+    class CreditMemoAddRs
+      attr_reader :records
+
+      def initialize(records)
+        @records = records
+      end
+
+      def handle_error(errors, config)
+        errors.each do |error|
+          Persistence::Object.handle_error(config,
+                                           error.merge(context: 'Adding Credit Memo'),
+                                           'creditmemos',
+                                           error[:request_id])
+        end
+      end
+
+      def process(config)
+        return { 'statuses_objects' => nil } if records.empty?
+
+        memos = []
+        records.each do |object|
+          memos << { 
+            creditmemos: {
+              id: object['TxnId'],
+              list_id: object['TxnId'],
+              edit_sequence: object['EditSequence']
+            }
+          }
+        end
+
+        Persistence::Object.update_statuses(config, products)
+      end
+
+
+    end
+  end
+end
