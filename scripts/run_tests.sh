376c808fa24d0c96bde1b257812a52ff62f1634d
Joaquín Pérez <mondongo@gmail.com>
2021-04-29 15:52:48 -0300

    Changed OS to Alpine. Cleaned up Docker and scripts. Updated gem versions. (#205)
    Co-authored-by: Joaquin Perez <joaquin@nurelm.com>

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-docker-compose -f docker-compose.yml \
-               -f docker-compose.dev.yml \
-               run --rm quickbooks-desktop-integration \
+docker-compose run --rm quickbooks-desktop-integration \
                sh -c "AWS_ACCESS_KEY_ID=key AWS_SECRET_ACCESS_KEY=secret AWS_REGION=us-east-1 bundle exec rspec ${@}"
